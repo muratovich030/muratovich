952067800c9d199c99cb1a0dd777e5588bf00559
Behruz <behruzbahtiyorov10@gmail.com>
2025-04-08 17:17:38 +0500

    make site adaptive

--- src/pages/login/ui.tsx
+++ src/pages/login/ui.tsx
@@ -43,7 +43,7 @@ const LoginPage = () => {
       </div>
       <div className={styles.loginRight}>
         <OutsiderLogin buttonText={"Login with Google"} imageUrl={"./google-logo.png"} />
-        <h5 >Welcome to <span>United Kingdom</span></h5>
+        <h5 className={styles.loginRightTitle}>Welcome to <span className={styles.loginRightTitleMark}>United Kingdom</span></h5>
         <span className={styles.separator}>OR</span>
         <LoginForm />
       </div>
